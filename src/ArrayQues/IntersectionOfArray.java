116843c487f47768b36fca3ce87ce806a499d8ca
awais-ansari <awaisansari1502@outlook.com>
2025-03-09 22:44:24 +0530

    optimized the code

--- src/ArrayQues/IntersectionOfArray.java
+++ src/ArrayQues/IntersectionOfArray.java
@@ -5,31 +5,38 @@ public class IntersectionOfArray {
     // find common elements in two arrays
     // arrays are sorted
     public static void main(String[] args) {
-        int arr1[] = {1, 2, 3};
-        int arr2[] = {3, 5};
+        int arr1[] = {1, 2, 3, 5 , 5};
+        int arr2[] = {3, 5, 5};
 
         intersectionArr(arr1, arr2);
     }
 
     public static void intersectionArr(int arr1[], int arr2[]){
 
-        for(int i=0; i<arr1.length; i++){
-            int element = arr1[i];
-            for(int j=0; j<arr2.length; j++){
-                if(element < arr2[j]){
-                    break;
-                };
-                if(element == arr2[j]){
-                    System.out.println(element);
-                    arr2[j] = Integer.MIN_VALUE;
-                    break;
-                }
+        int n = arr1.length;
+        int m = arr2.length;
+        int  i=0;
+        int  j=0;
+
+        while(i<n && j<m){
+            if(arr1[i] == arr2[j]){
+                System.out.print(" "+arr1[i]);
+                i++;
+                j++;
+            }
+            else if(arr1[i] < arr2[j]){
+                i++;
+            }
+            else{
+                j++;
             }
         }
 
         return;
 
     }
+    
+    
     //  print array fn
      public static void printArr(int arr[]){
         for(int i : arr){
